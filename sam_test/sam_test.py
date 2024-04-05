618a51df040bcf87252dd24568802a01e4150176
Kai Zhu <zhuk2@mcmaster.ca>
2024-04-04 22:33:03 -0400

    added multi-point input selection

--- sam_test/sam_test.py
+++ sam_test/sam_test.py
@@ -4,11 +4,16 @@ import cv2
 import numpy as np
 import torch
 
+# each click adds a point, close first drawn window to generate mask
 def onclick(event):
     global input_point
-    input_point = np.array([[event.xdata, event.ydata]])
-    print(f'Updated input_point to: {input_point}')
-    plt.close()
+    global input_label
+    input_point = np.append(
+        input_point, np.array([[event.xdata, event.ydata]]), axis=0)
+    input_label = np.append(input_label, 1)
+    plt.plot(event.xdata, event.ydata, 'x')
+    print(f'input points: {input_point}')
+    plt.draw()
 
 image = cv2.imread('./truck.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -23,8 +28,8 @@ sam.to(device)
 predictor = SamPredictor(sam)
 predictor.set_image(image)
 
-input_point = np.array([[500, 375]])
-input_label = np.array([1])
+input_point = np.empty((0, 2), float)
+input_label = np.array([], int)
 
 # click to choose input position
 fig, ax = plt.subplots(figsize=(10,10))
@@ -33,24 +38,32 @@ plt.imshow(image)
 plt.show()
 fig.canvas.mpl_disconnect(cid)
 
-masks, _, _ = predictor.predict(
+masks, scores, logits = predictor.predict(
     point_coords=input_point,
     point_labels=input_label,
     # multimask_output=False,
 )
 
-print(masks.shape)
+
+print("mask shape: ", masks.shape)
+print("score shape: ", scores.shape)
+print("logits shape: ", logits.shape)
+
+print("Scores: ", scores)
 
 for i, mask in enumerate(masks):
     plt.figure(figsize=(12, 6))
 
     plt.subplot(1, 2, 1)
-    plt.plot(input_point[0, 0], input_point[0, 1], 'x')
+    plt.title(f"Score: {scores[i]}")
+    plt.plot(input_point[:, 0], input_point[:, 1], 'x')
     plt.imshow(mask[:,:,np.newaxis] * image)
     plt.axis('off')
 
+
     plt.subplot(1, 2, 2)
-    plt.plot(input_point[0, 0], input_point[0, 1], 'x')
+    plt.title("BEST SCORE!" if scores[i] == scores.max() else "")
+    plt.plot(input_point[:, 0], input_point[:, 1], 'x')
     plt.imshow(mask, cmap='gray')
     plt.axis('off')
 
